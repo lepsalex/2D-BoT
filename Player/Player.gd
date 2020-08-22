92a6d3d72a1eed21d10860cf2ee05df69aab2eaa
Alex <lepsalex@gmail.com>
2020-08-22 15:57:09 -0400

    some world stuff

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,6 +9,9 @@ var velocity = Vector2.ZERO
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
