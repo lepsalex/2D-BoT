6ec75620a2adf4a40dbf4209d056d1cfb23afcbc
Alex <lepsalex@gmail.com>
2020-08-25 13:23:35 -0400

    some ui, trees, and shadows

---
+++ UI/HealthUI.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var hearts = 4 setget set_hearts
+var max_hearts = 4 setget set_max_hearts
+
+onready var heartUIEmpty = $HeartUIEmpty
+onready var heartUiFull = $HeartUIFull
+
+func set_hearts(value):
+	hearts = clamp(value, 0, max_hearts)
+	if heartUiFull != null:
+		heartUiFull.rect_size.x = hearts * 15
+
+func set_max_hearts(value):
+	max_hearts = max(value, 1)
+	self.hearts = min(hearts, max_hearts) # don't know that this is needed?
+	if heartUIEmpty != null:
+		heartUIEmpty.rect_size.x = hearts * 15
+
+func _ready():
+	self.max_hearts = PlayerStats.max_health
+	self.hearts = PlayerStats.health
+	PlayerStats.connect("health_change", self, "set_hearts")
+	PlayerStats.connect("max_health_changed", self, "set_max_hearts")
