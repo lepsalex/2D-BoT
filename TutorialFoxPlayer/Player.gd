117acb5b5f202a1773750f8b0274aa5a57c8acc5
Alex <lepsalex@gmail.com>
2020-08-31 17:49:50 -0400

    starting to layer in BoT elements

--- TutorialFoxPlayer/Player.gd
+++ TutorialFoxPlayer/Player.gd
@@ -4,31 +4,42 @@ const PlayerHurtSound = preload("res://Player/PlayerHurtSound.tscn")
 
 export(int) var ACCELERATION = 480
 export(int) var MAX_SPEED = 80
+export(int) var ROLL_SPEED = 120
 export(int) var FRICTION = 520
+export(float) var ROLL_COEFFICIENT = 0.8
 export(int) var INVINCIBILITY_TIMER = 0.6
 
 enum {
-	MOVE
+	MOVE,
+	ROLL,
+	ATTACK
 }
 
 var state = MOVE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.DOWN
 var stats = PlayerStats
 
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var swordHitbox = $HitBoxPivot/SwordHitBox
 onready var hurtbox = $HurtBox
 onready var blinkAnimPlayer = $BlinkAnimationPlayer
 
 func _ready():
 	stats.connect("no_health", self, "queue_free")
 	animationTree.active = true
+	swordHitbox.knockback_vector = roll_vector
 
 
 func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
+		ROLL:
+			roll_state(delta)
+		ATTACK:
+			attack_state(delta)
 
 
 func move_state(delta):
@@ -38,6 +49,8 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
+		swordHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
@@ -47,12 +60,39 @@ func move_state(delta):
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-		
+
 	move()
+	
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+
+func roll_state(_delta):
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
+
+
+func attack_state(_delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+	# todo: animation cancelling
 
 
 func move():
 	velocity = move_and_slide(velocity)
+
+
+func roll_anim_finished():
+	velocity = velocity * ROLL_COEFFICIENT
+	state = MOVE
+
+
+func attack_anim_finished():
+	state = MOVE
 
 
 func play_hurt_sound():
---
+++ TutorialFoxPlayer/SwordHitbox.gd
@@ -0,0 +1,3 @@
+extends "res://Interactions/HitBox.gd"
+
+var knockback_vector = Vector2.ZERO
